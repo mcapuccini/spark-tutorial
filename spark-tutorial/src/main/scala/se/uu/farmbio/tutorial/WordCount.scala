b7e5b3440a78d36014548da60fd90333b601c3f9
marco <marco@student94-43.eduroam.uu.se>
2015-12-04 14:12:06 +0100

    add comments and stop the context in the end

--- spark-tutorial/src/main/scala/se/uu/farmbio/tutorial/WordCount.scala
+++ spark-tutorial/src/main/scala/se/uu/farmbio/tutorial/WordCount.scala
@@ -4,24 +4,30 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
 object WordCount {
-  
+
   def main(args: Array[String]) = {
-    
+
+    //Start the Spark context
     val conf = new SparkConf()
       .setAppName("WordCount")
       .setMaster("local")
     val sc = new SparkContext(conf)
-    
+
+    //Read some example file to a test RDD
     val test = sc.textFile("food.txt")
-    test.flatMap { line =>
-      line.split(" ")
+
+    test.flatMap { line => //for each line
+      line.split(" ") //split the line in word by word. NB: we use flatMap, because we return a list
     }
-    .map { word => 
-      (word,1)
+    .map { word => //for each word
+      (word,1) //Return a key/value tuple, with the word as key and 1 as value
     }
-    .reduceByKey(_ + _)
-    .saveAsTextFile("food.count.txt")
-    
+    .reduceByKey(_ + _) //Sum all of the value with same key
+    .saveAsTextFile("food.counts.txt") //Save to a text file
+
+    //Stop the Spark context
+    sc.stop
+
   }
 
 }
